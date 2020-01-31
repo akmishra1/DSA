59266d01c18067bdabe1e7544f6c9dedf799d2a6
Akash Mishra <akashmishra@Akashs-MBP.example.com>
2020-01-31 20:01:40 +0530

    Tower Of Hanoi

---
+++ src/com/learnbay/Stack/TowerOfHanoi.java
@@ -0,0 +1,26 @@
+package com.akash.stack;
+
+public class TOwerOfHanoi
+
+{
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int n = 6; // Number of disks 
+        towerOfHanoi(n, 'A', 'C', 'B'); 
+	}
+	
+	private static void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod) 
+    { 
+        if (n == 1) 
+        { 
+            System.out.println("Move disk 1 from rod " +  from_rod + " to rod " + to_rod); 
+            return; 
+        } 
+        towerOfHanoi(n-1, from_rod, aux_rod, to_rod); 
+        System.out.println("Move disk " + n + " from rod " +  from_rod + " to rod " + to_rod); 
+        towerOfHanoi(n-1, aux_rod, to_rod, from_rod); 
+    } 
+
+}
